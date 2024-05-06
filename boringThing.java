7f4b0f9e698c415ebea4ad197eedd84006624835
Dinh Nhat <mkoijnnguyen@gmail.com>
2024-05-06 00:29:52 -0500

    add date

--- boringThing.java
+++ boringThing.java
@@ -2,6 +2,7 @@ public class boringThing {
     public static void main(String[] args) {
 
         System.out.println("This is a really boring file!!");
+        System.out.println("May 05");
         int boringLevel = 1;
     }
 }
