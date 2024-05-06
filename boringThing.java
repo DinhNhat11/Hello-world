0b0b7dacbb9b04dc499c64c0ac4e226df41a8e96
Dinh Nhat <mkoijnnguyen@gmail.com>
2024-05-06 01:23:59 -0500

    add r

--- boringThing.java
+++ boringThing.java
@@ -1,7 +1,7 @@
 public class boringThing {
     public static void main(String[] args) {
 
-        System.out.println("TTTTTTTTTTThis is a really boring file!!!");
+        System.out.println("TTTTTTTTTTThis is a rrrrrrrrrreally boring file!!!");
         System.out.println("About git");
         int boringLevel = 1;
     }
